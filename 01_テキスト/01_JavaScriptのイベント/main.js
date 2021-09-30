06ff7426db8116c983ae2fa6f5b2f55df136963e
natsuriku159 <natsuriku159@outlook.jp>
2021-09-30 20:43:40 +0900

    DOM編

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,44 @@
+const button = document.getElementById("button")
+
+//button要素とクリックイベント
+//const alertMessage = function() {
+//    alert("クリックしたね。。。")
+//}
+
+//alertMessage()//アラートが表示される
+//button.onclick = alertMessage
+
+//button.onclick = function() {
+//    alert("クリックしたね。。。")
+//}
+//機能する
+
+// const alertMessage = function() {
+//    alert("クリックしたね。。。")
+// }
+
+// button.onclick = alertMessage()
+//機能せず。。
+
+// input要素とinputイベント
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+// const sayHello = function() {
+    // console.log("hello")
+// }
+
+// inputText.oninput = sayHello
+// inputDate.oninput = sayHello
+
+// const logValue = function(e) {
+    // console.log(e.target.value)
+// }
+
+// inputText.oninput = logValue
+// inputDate.oninput = logValue
+
+// document.onkeydown = function(e) {
+    // console.log(e.key)
+// }
+
