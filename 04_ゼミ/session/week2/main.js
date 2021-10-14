7e51d413da4f8719a8bf10e7f46572e75f7ca8aa
natsuriku159 <natsuriku159@outlook.jp>
2021-10-14 18:39:57 +0900

    deleteButtonまで

---
+++ 04_ゼミ/session/week2/main.js
@@ -0,0 +1,29 @@
+const memoInput = document.getElementById("memo-input")
+const addButton = document.getElementById("add-button")
+const memoContainer = document.getElementById("memo-container")
+
+addButton.onclick = function() {
+    //console.dir(memoInput) //valueでmemoInputの値を取得できる
+    //console.log(memoInput.value)
+    const text = memoInput.value
+
+    const card = document.createElement("div")
+    card.className = "card"
+
+    const content = document.createElement("div")
+    content.className = "content"
+    content.textContent = text
+    card.append(content)
+
+    const deleteButton = document.createElement("div")
+    deleteButton.className = "delete"
+
+    deleteButton.onclick = function() {
+        card.remove()
+    }
+    card.append(deleteButton)
+
+    memoContainer.append(card)
+
+    memoInput.value = ""
+}
